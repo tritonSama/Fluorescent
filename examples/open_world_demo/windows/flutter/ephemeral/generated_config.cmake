4fe1a235fe82c6ecbd7a9027e2f1b0aef9338fd3
tritonSama <97619606+tritonSama@users.noreply.github.com>
2026-09-16 21:19:17 -0500

    /

---
+++ examples/open_world_demo/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\blue-\\projects\\Fluorescent\\examples\\open_world_demo" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\blue-\\projects\\Fluorescent\\examples\\open_world_demo"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\blue-\\projects\\Fluorescent\\examples\\open_world_demo\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\blue-\\projects\\Fluorescent\\examples\\open_world_demo"
+  "FLUTTER_TARGET=C:\\Users\\blue-\\projects\\Fluorescent\\examples\\open_world_demo\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9CVUlMRF9OQU1FPTAuMS4w,RkxVVFRFUl9WRVJTSU9OPTMuNDcuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OTU4NGM2NzEzYg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MDZhMmUyYTExMA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMy4z"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\blue-\\projects\\Fluorescent\\examples\\open_world_demo\\.dart_tool\\package_config.json"
+)
